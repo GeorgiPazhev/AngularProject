2ef1ea0b1a524e1446cc3906b7f304cf31c1a314
Georgi Pazhev <georgpajev@gmail.com>
2024-12-07 19:29:36 +0200

    Auth guard added

--- flightCargo/src/app/app.routes.ts
+++ flightCargo/src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { PlanesComponent } from './planes/planes.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ShipmentsComponent } from './shipments/shipments.component';
 import { FlightsComponent } from './flights/flights.component';
+import { AuthGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
     {path:"", redirectTo:"/home", pathMatch:"full"},
     {path:"home", component:HomeComponent},
     {path:"flights", component:FlightsComponent},
-    {path:"planes", component:PlanesComponent},
-    {path:"shipments", component:ShipmentsComponent},
+    {path:"planes", component:PlanesComponent, canActivate:[AuthGuard]},
+    {path:"shipments", component:ShipmentsComponent, canActivate:[AuthGuard]},
     {path:"**", component:PageNotFoundComponent}
 ];
