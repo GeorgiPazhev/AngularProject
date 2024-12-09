a9d32c8e14fe1c17c988b6c0fce1af8568124191
Georgi Pazhev <georgpajev@gmail.com>
2024-12-09 20:10:26 +0200

    Fixing flight and flights components

--- flightCargo/src/app/flights/flight/flight.component.ts
+++ flightCargo/src/app/flights/flight/flight.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Flight } from '../../../types/Flight';
 
 @Component({
@@ -8,6 +8,20 @@ import { Flight } from '../../../types/Flight';
   templateUrl: './flight.component.html',
   styleUrl: './flight.component.css'
 })
-export class FlightComponent {
+export class FlightComponent implements OnInit{
     @Input('flight') singleFlight:Flight|null = null;
+    filledPayload:number = 0;
+    filledVolume:number = 0;
+    
+    ngOnInit(): void {
+      
+      const overallPayload:number = typeof this.singleFlight?.aircraft.payload =='undefined' ? 1 : this.singleFlight?.aircraft.payload;
+      const overallVolume:number = typeof this.singleFlight?.aircraft.volume =='undefined' ? 1 : this.singleFlight?.aircraft.volume;
+      
+      this.singleFlight?.shipments?.forEach((currentShipment)=> {this.filledPayload += currentShipment.weight});
+      this.singleFlight?.shipments?.forEach((currentShipment)=> {this.filledVolume += currentShipment.volume});
+      this.filledPayload = (this.filledPayload / overallPayload) * 100;
+      this.filledVolume = (this.filledVolume / overallVolume) * 100;
+    }
+
 }
