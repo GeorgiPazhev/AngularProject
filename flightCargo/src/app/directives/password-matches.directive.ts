de4df9735e6f7744deee3a2227346654c0f0864e
Georgi Pazhev <georgpajev@gmail.com>
2024-12-10 10:35:27 +0200

    Fixing match password directive

--- flightCargo/src/app/directives/password-matches.directive.ts
+++ flightCargo/src/app/directives/password-matches.directive.ts
@@ -1,35 +1,42 @@
 import { Directive, Input } from '@angular/core';
-import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
   selector: '[appPasswordMatches]',
-  standalone: true
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      multi: true,
+      useExisting: PasswordMatchesDirective,
+    },
+  ],
 })
 export class PasswordMatchesDirective implements Validator{
 
-  @Input('') passwordControlNames:string[]|null = null;
+  @Input() appPasswordMatches:string[] = [];
   
 
   constructor() { }
 
   validate(control: AbstractControl): ValidationErrors | null {
     let passwordControls:Array<AbstractControl|null> = new Array();
-    this.passwordControlNames?.forEach((passControlName) => {
+    this.appPasswordMatches?.forEach((passControlName) => {
       if (control.get(passControlName) != null) 
         passwordControls.push(control.get(passControlName));
     });
     
     let passValuesMatch:boolean = true;
     const passNotMatch = { matchPasswordsValidator: true };
-
+    
     if (passwordControls.length < 2)
     {
       return passNotMatch;
     }
-
+    
     for(let index = 1; index<passwordControls.length; index++)
     {
-      passValuesMatch = passValuesMatch && passwordControls[index-1]?.value == passwordControls[index]?.value;
+      passValuesMatch = passValuesMatch && (passwordControls[index-1]?.value === passwordControls[index]?.value);
     }
 
     return passValuesMatch ? null : passNotMatch;
--- flightCargo/src/app/user/register-new-user/register-new-user.component.ts
+++ flightCargo/src/app/user/register-new-user/register-new-user.component.ts
@@ -3,17 +3,21 @@ import { FormsModule, NgForm } from '@angular/forms';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
 import { EmailDirective } from '../../directives/email.directive';
+import { PasswordMatchesDirective } from '../../directives/password-matches.directive';
 
 @Component({
   selector: 'app-register-new-user',
   standalone: true,
-  imports: [FormsModule, EmailDirective],
+  imports: [FormsModule, EmailDirective, PasswordMatchesDirective],
   templateUrl: './register-new-user.component.html',
   styleUrl: './register-new-user.component.css'
 })
 export class RegisterNewUserComponent {
 
   @ViewChild("registerForm") registerForm:NgForm | undefined;  
+
+  passControlNames:string[] = ['password','retypePassword'];
+
    constructor(private userService:UserService, private router:Router){}
 
 
